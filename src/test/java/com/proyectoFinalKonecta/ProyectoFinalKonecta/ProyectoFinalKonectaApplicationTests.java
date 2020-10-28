c72cdff28f05338f4ad0409ed360b978c4b9d37c
danielaguirrec <35563201+danielaguirrec@users.noreply.github.com>
2020-10-28 00:11:58 -0500

    Barra de navegacion

---
+++ src/test/java/com/proyectoFinalKonecta/ProyectoFinalKonecta/ProyectoFinalKonectaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoFinalKonecta.ProyectoFinalKonecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalKonectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
